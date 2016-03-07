330c0bfa69502317cd2a8f061104d8d6a02ca564
Si Kai Lee <a_technicolor_skye@hotmail.com>
2016-03-06 23:05:49 -0500

    Checked number of nodes for FB and PP networks

---
+++ example/Svinet_Process.py
@@ -0,0 +1,40 @@
+'''
+Process protein networks for Svinet
+Essentially the same as CESNA_Process, but displays number of nodes
+'''
+
+
+def Proc_Svinet(Net):
+    with open(Net, 'r') as I_Net:
+        Input_Net = I_Net.readlines()
+
+    node_counter = 1
+
+    Nodes = {}
+
+    Output = set()
+
+    for line in Input_Net[1:]:
+        line_s = line.split('\t')
+        if line_s[0] not in Nodes:
+            Nodes[line_s[0]] = node_counter
+            node_counter += 1
+        if line_s[1] not in Nodes:
+            Nodes[line_s[1]] = node_counter
+            node_counter += 1
+        # print str(Nodes[line_s[0]]) + '\t' + str(Nodes[line_s[1]]) + '\n'
+        Output.update([str(Nodes[line_s[0]]) + '\t' + str(Nodes[line_s[1]]) + '\n'])
+        # print Output
+
+    # Need to know number of nodes for Svinet, checked 5197 for pp network and 1494 for 1912.edges
+    print node_counter - 1
+
+    # Output = sorted(Output)
+
+    with open('pp_networks_num.txt', 'w') as O:
+        for edge in Output:
+            O.write(edge)
+
+
+if __name__ == '__main__':
+    Proc_Svinet('networks.txt')
